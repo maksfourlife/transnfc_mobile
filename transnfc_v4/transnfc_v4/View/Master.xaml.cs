438eb76d5f2311cc797100bc57f157bbf1639fe5
maxkrutonog <maksfourlife@gmail.com>
2020-03-04 18:39:48 +0300

    Added function to switch pages to master

--- transnfc_v4/transnfc_v4/View/Master.xaml.cs
+++ transnfc_v4/transnfc_v4/View/Master.xaml.cs
@@ -16,7 +16,30 @@ namespace transnfc_v4.View
         {
             InitializeComponent();
 
+            BindingContext = new ViewModel.Master(NewPageSelected);
+
             SetPage(new Main());
+        }
+
+        void NewPageSelected(string page_name)
+        {
+            switch (page_name)
+            {
+                case "Main":
+                    SetPage(new Main());
+                    return;
+                case "Wallet":
+                    SetPage(new Wallet());
+                    return;
+                case "Settings":
+                    SetPage(new Settings());
+                    return;
+                case "Feedback":
+                    SetPage(new Feedback());
+                    return;
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         void SetPage(Page page)
@@ -25,6 +48,7 @@ namespace transnfc_v4.View
             {
                 BarBackgroundColor = (Color)Application.Current.Resources["blue1"],
             };
+            IsPresented = false;
         }
     }
 }
---
+++ transnfc_v4/transnfc_v4/ViewModel/Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace transnfc_v4.ViewModel
+{
+    class Master : INotifyPropertyChanged
+    {
+        public Master(Action<string> new_page_selected)
+        {
+            NewPageSelected = new Command<string>(new_page_selected);
+        }
+
+        public Command<string> NewPageSelected { private set; get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string property_name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+        }
+    }
+}
